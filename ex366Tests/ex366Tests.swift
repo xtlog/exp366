5c3d159396c013919a7c2f349390a543831097f9
xtlog <xtlog@xtlogdeMacBook-Pro.local>
2015-10-21 02:10:24 -0700

    Initial Commit

---
+++ ex366Tests/ex366Tests.swift
@@ -0,0 +1,36 @@
+//
+//  ex366Tests.swift
+//  ex366Tests
+//
+//  Created by xtlog on 15/10/21.
+//  Copyright (c) 2015年 sxnic. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class ex366Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
